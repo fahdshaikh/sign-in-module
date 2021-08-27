0b9f0719670cb0dec94b84d688402019d16b4a74
utpal.maji <utpal.maji@orchids.edu.in>
2021-08-27 14:14:26 +0530

    modify

--- src/pages/otherPage/index.jsx
+++ src/pages/otherPage/index.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from "react";
+import CTabA from "../../components/CTabA";
+import tabData from "./tabData.js";
 
 const OtherComponent = () => {
+  const [activeTab, setActiveTab] = useState("Maths");
   return (
     <div>
-      <h1>Another Page</h1>
+      <CTabA data={tabData} activeTab={activeTab} setActiveTab={setActiveTab} />
     </div>
-  )
-}
+  );
+};
 
-export default OtherComponent
+export default OtherComponent;
